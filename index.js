6807167fadd1839e93485eb0344bb87412a251a6
FlatHippo <47155251+FlatHippo@users.noreply.github.com>
2023-01-21 15:30:19 -0500

    WE HAVE MOVEMENT (im not seeing enough)

--- index.js
+++ index.js
@@ -7,27 +7,74 @@ const backgroundImg = new Image();
 backgroundImg.src = './images/background.jpg';
 const playerImg = new Image();
 playerImg.src = './images/player.png';
+let playerSpeed = 2.5;
 //#endregion
 
 
 class Sprite{
-    constructor({position, image, scale}){
+    constructor({position, startPositon, image, scale, rotation = 0}){
         this.position = position;
         this.image = image;
         this.scale = scale;
+        this.startPositon = startPositon;
+        this.rotation = rotation;
+        this.rotationSpeed = 360 * Math.PI / 180;
+        this.rotating = false;
     }
 
     draw(){
-        //if the sprite has a scale, adapt
-        if(this.scale != null){
-            context.drawImage(this.image, this.position.x, this.position.y, this.image.width * this.scale.x, this.image.height * this.scale.y);
-        }
-        else{
-            context.drawImage(this.image, this.position.x, this.position.y);
-        }
+        context.save();
+        context.translate(this.position.x + this.image.width/2, this.position.y + this.image.height/2);
+        context.rotate(this.rotation);
+        context.drawImage(this.image, -this.image.width/2, -this.image.height/2, this.image.width * this.scale.x, this.image.height * this.scale.y);
+        context.restore();
     }
-
+    rotate(angle){
+        this.rotation += angle * Math.PI / 180;
+    }
 };
+class Boundary{
+    constructor({position, width, height}){
+        this.position = position;
+        this.width = width;
+        this.height = height;
+    }
+    draw(){
+        context.fillStyle = 'red';
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+class Timer{
+    static countdown = 10;
+    constructor(){
+        this.activateTimer();
+    }
+    activateTimer(){
+        setInterval(this.randomizePlayerSpeed, Timer.countdown * 1000);
+    }
+    randomizePlayerSpeed(){
+        let newSpeed;
+        do{
+            newSpeed = Math.random() * 7 - 2;
+        }
+        while((newSpeed >= -0.5 && newSpeed <= 0.5) || (playerSpeed - newSpeed <= 0.8 && playerSpeed - newSpeed >= -0.8));
+        player.rotating = true;
+        playerSpeed = 0;
+        setTimeout(() => {player.rotating = false
+                          playerSpeed = newSpeed}, 1000);
+        ;
+    }
+}
+
+function isColliding(sprite, boundary){
+    if(sprite.position.x + sprite.image.width >= boundary.position.x && 
+        sprite.position.x <= boundary.position.x + boundary.width && 
+        sprite.position.y + sprite.image.height >= boundary.position.y && 
+        sprite.position.y <= boundary.position.y + boundary.height){
+            console.log("collid");
+            return true;
+    }
+}
 
 const background = new Sprite(
     {position: {x: 0, y: 0},
@@ -37,10 +84,17 @@ const background = new Sprite(
 const player = new Sprite(
     {
         position: {x: 200, y: 350},
+        startPositon: {x: 200, y: 350},
         image: playerImg,
         scale: {x: 1, y: 1}
     });
-
+const testBoundary = new Boundary(
+    {
+        position: {x: 400, y: 400},
+        width: 100,
+        height: 100
+    });
+const playerSpeedRandomizer = new Timer();
 const keys = {
     w: {
         pressed: false
@@ -56,11 +110,21 @@ const keys = {
     }
 
 };
+let previousTime = Date.now();
 function Update(){
-    let playerSpeed = 3;
     window.requestAnimationFrame(Update);
     background.draw();
+    testBoundary.draw();
     player.draw();
+    let currentTime = Date.now();
+    let deltaTime = (currentTime - previousTime) / 1000;
+    if(player.rotating){
+        player.rotation += player.rotationSpeed * deltaTime;
+    }
+    else{
+        player.rotation = 0 * Math.PI / 180;
+    }
+    previousTime = currentTime;
     if(keys.w.pressed){
         player.position.y -= playerSpeed;
     }
@@ -72,6 +136,10 @@ function Update(){
     }
     if(keys.s.pressed){
         player.position.y += playerSpeed;
+    }
+    if(isColliding(player, testBoundary)){
+        player.position = {x: player.startPositon.x,
+                           y: player.startPositon.y};
     }
 }
 
